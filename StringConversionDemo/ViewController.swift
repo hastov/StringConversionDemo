9519d8a666a6ad17d751bb1c4e01b4a6f278a958
hristoh <hristo.hristov@mkodo.com>
2022-11-20 10:04:49 +0200

    Initial and target textfields to demo package

--- StringConversionDemo/ViewController.swift
+++ StringConversionDemo/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import StringConversion
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var initialTextField: UITextField!
+    @IBOutlet private weak var targetTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialTextField.addTarget(
+            self,
+            action: #selector(textFieldEditingChanged(_:)),
+            for: .editingChanged
+        )
+        targetTextField.addTarget(
+            self,
+            action: #selector(textFieldEditingChanged(_:)),
+            for: .editingChanged
+        )
+        showConversionResult()
     }
 
+    private func showConversionResult() {
+        let color = StringConversion().can(
+            initialTextField.text ?? "",
+            beConvertedInto: targetTextField.text ?? ""
+        ) ? UIColor.green.cgColor : UIColor.red.cgColor
+        targetTextField.layer.borderWidth = 1
+        targetTextField.layer.borderColor = color
+        initialTextField.layer.borderWidth = 1
+        initialTextField.layer.borderColor = color
+    }
+}
 
+extension ViewController {
+    @objc private func textFieldEditingChanged(_ textField: UITextField) {
+        showConversionResult()
+    }
 }
 
